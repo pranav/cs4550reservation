15c3dbfc3a332d5a20d23494f8336425a23e1ce3
Pranav Gandhi <hyfi@ccs.neu.edu>
2013-02-21 19:09:25 -0500

    Loads things from the data.json file' '

--- js/conference-room.js
+++ js/conference-room.js
@@ -5,7 +5,7 @@ function add_conference_room(room){
 
 function add_form_conference_rooms(){
   $('.conference-room').each(function(k,v){
-    $(this).append('<form class="form-inline" action=process.php method=POST><h3>Reserve</h3><input class="input-small" type="text" name="date" placeholder="Date"> <input  class="input-small" type="text" name="time" placeholder="Time"> <a href="#" class="btn btn-primary">Submit</a></form>');
+    $(this).append('<h3>Reserve</h3><form class="form-inline conference-form" action=process.php method=POST><input class="date span2" type="date" name="date" placeholder="Date"> <input  class="input-small" type="time" name="time" placeholder="Time"> <input class="btn btn-primary" type="submit" value="Submit"></form>');
   });
 }
 
@@ -15,4 +15,10 @@ $.getJSON("data.json", function(data){
     add_conference_room(data.rooms[i]);
   }
   add_form_conference_rooms();
+  $('.conference-form').submit(function(e){
+    e.preventDefault();
+    var date = $($(this).children()[0]).val();
+    var time = $($(this).children()[1]).val();
+    $(this).append('<p>You entered: '+date+' at '+time+'.</p>');
+  });
 });
